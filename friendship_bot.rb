5ee1e2f5422d795eb9faab64238488605e9e38f5
Netto <nettofarah@gmail.com>
2011-10-05 09:19:26 -0300

    enabled friendship bot. already accepting friends :)

---
+++ friendship_bot.rb
@@ -0,0 +1,26 @@
+require 'mechanize'
+
+class FriendshipBot
+  
+  def accept_friends username, password
+    @agent = Mechanize.new
+    requests_page = login(username, password)
+    
+    friendship_request = requests_page.forms_with(:action => '/ajax/reqs.php').each do |request|
+      request.click_button
+    end
+  end
+
+  private
+  def login(username, password)
+    page = @agent.get('http://facebook.com/reqs.php')
+    form = page.form_with :id => 'login_form'
+
+    form.email = username
+    form.pass = password
+
+    form.submit
+  end
+  
+end
+
