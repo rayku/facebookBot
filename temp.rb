c1722714fe9c9a20eb8adbcfdcae326f5edf65e9
Netto <nettofarah@gmail.com>
2011-10-06 23:17:54 -0300

    removing old spike

--- temp.rb
+++
@@ -1,7 +0,0 @@
-require 'blather/client'
-
-setup 'raykubot@chat.facebook.com', 'bghtyu123'
-
-message :chat?, :body do |m|
-  write_to_stream Blather::Stanza::Message.new(m.from, "I'm just a bot. You cannot talk to me.")
-end
