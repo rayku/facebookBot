767ddc551f8186e1587117d86a9e92b425e94966
Netto <nettofarah@gmail.com>
2011-10-11 20:33:28 -0300

    fixing dependencies in spec

--- spec/rayku_bot_spec.rb
+++ spec/rayku_bot_spec.rb
@@ -1,19 +1,19 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe 'RaykuBot' do
-  
+
   context "returning the online tutors" do
     it "should use MiniFB to perform the query" do
       MiniFB.should_receive(:fql)
       RaykuBot.tutors
     end
-    
+
     it "should send the correct access_token and the correct query" do
-      credentials = YAML.load_file('configs.yml')[:facebook]
+      credentials = Facebook::Config
       query = "SELECT uid, name, pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me()) AND online_presence != 'offline'"
-      MiniFB.should_receive(:fql).with(credentials[:access_token], query)
+      MiniFB.should_receive(:fql).with(credentials::ACCESS_TOKEN, query)
       RaykuBot.tutors
     end
   end
-  
+
 end
