61f908cc06cd745fec22f8661dd4cb494794d706
Netto <nettofarah@gmail.com>
2011-10-11 20:20:36 -0300

    adding configuration as class

---
+++ configs/facebook.rb
@@ -0,0 +1,8 @@
+module Facebook
+  module Config
+    USERNAME = {:login => 'raykubot@rayku.com', :chat => 'raykubot@chat.facebook.com'}
+    PASSWORD = 'bghtyu123'
+    ACCESS_TOKEN = 'AAAB3EIZBtPSkBALxoVyNobQqiTTQAbWt6kFPzGhXFpGcD9s6EwIXmmHIW1qujdABeBluXOzYk9sn6kVxUbcNq6uR2ZAYbLutZBhhABFkgZDZD'
+    APP_ID = '130913013677353'
+  end
+end
