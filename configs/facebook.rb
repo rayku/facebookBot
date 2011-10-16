0b476dfe23f7d67ba9788d823528480f52c15a1e
Donny <heydonny@kinkarso.com>
2011-10-16 00:57:57 -0400

    adding new API key

--- configs/facebook.rb
+++ configs/facebook.rb
@@ -3,6 +3,6 @@ module Facebook
     USERNAME = {:login => 'raykubot@rayku.com', :chat => 'raykubot@chat.facebook.com'}
     PASSWORD = 'bghtyu123'
     ACCESS_TOKEN = 'AAAB3EIZBtPSkBALxoVyNobQqiTTQAbWt6kFPzGhXFpGcD9s6EwIXmmHIW1qujdABeBluXOzYk9sn6kVxUbcNq6uR2ZAYbLutZBhhABFkgZDZD'
-    APP_ID = '130913013677353'
+    APP_ID = '304330886250108'
   end
 end
